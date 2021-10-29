c52f902e71c03e1bb87d5e11afb4b58d4d8c1817
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-10-29 12:25:59 -0400

    patient info

--- js/data.js
+++ js/data.js
@@ -9,6 +9,12 @@ function getPatientName(pt) {
   }
 }
 
+function displayPatient(pt) {
+    document.getElementById('patient_name').innerHTML = getPatientName(pt);
+    document.getElementById('gender').innerHTML = pt.gender;
+    document.getElementById('dob').innerHTML = pt.birthDate;
+  }
+  
 FHIR.oauth2.ready().then(function(client) {
 
     // get patient object and then display its demographics info in the banner
