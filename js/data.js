ba810b221c48cb87a88f8edb59c4d8b38a5afe69
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-10-29 12:15:43 -0400

    JS

--- js/data.js
+++ js/data.js
@@ -8,3 +8,15 @@ function getPatientName(pt) {
     return "anonymous";
   }
 }
+
+FHIR.oauth2.ready().then(function(client) {
+
+    // get patient object and then display its demographics info in the banner
+    client.request(`Patient/${client.patient.id}`).then(
+      function(patient) {
+        displayPatient(patient);
+        console.log(patient);
+      }
+    )
+}
+)
