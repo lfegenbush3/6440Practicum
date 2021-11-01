83b806af106e369f623deba6209b05a264083f55
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-11-01 11:54:04 -0400

    Testing

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 function getPatientName(pt) {
   for(let i = 0; i <pt.name.length; i++){
     console.log(pt.name[i].use)
-    if (pt.name && pt.name[i].use != 'old') {
+    if (pt.name) {
       var names = pt.name.map(function(name) {
         return name.given.join(" ") + " " + name.family;
       });
