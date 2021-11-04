179bfb4ef85d55b34c3c0b1d73ad593f95ca9c11
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-11-04 13:01:14 -0400

    g

--- js/data.js
+++ js/data.js
@@ -84,7 +84,8 @@ FHIR.oauth2.ready().then(function(client) {
       console.log("weight:" + getQuantityValueAndUnit(weight[0]))
        
       for (i=0; i < weight.length; i++){
-        console.log("here")
+        console.log(getMaxDate(weight))
+        console.log(new Date(weight[i].effectiveDateTime))
         if (new Date(weight[i].effectiveDateTime) == getMaxDate(weight)){
           console.log(weight[i])
         }
