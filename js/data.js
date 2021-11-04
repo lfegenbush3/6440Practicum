0f1ee361ac196acc515c98e0f91e77ac10ca85a5
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-11-04 10:43:31 -0400

    test

--- js/data.js
+++ js/data.js
@@ -61,7 +61,7 @@ FHIR.oauth2.ready().then(function(client) {
       var byCodes = client.byCodes(ob, 'code');
       var flu_vaccine = byCodes('72058-1');
       var weight = byCodes('29463-7');
-
+      console.log(ob)
       // create patient object
       var p = defaultPatient();
 
