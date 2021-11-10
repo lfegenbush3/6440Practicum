9c043c436eda1dfaa58573c4a239b792ae870ee0
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-11-10 09:41:11 -0500

    j

--- js/data.js
+++ js/data.js
@@ -147,8 +147,8 @@ FHIR.oauth2.ready().then(function(client) {
       }
      
       //Adult health indicators and prevention data
-      console.log(patient.gender)
-      console.log(patient)
+      console.log(client.patient.gender)
+      console.log(client.patient)
       //Colon Cancer Screening
       var colon_cancer = byCodes('77353-1');
       
