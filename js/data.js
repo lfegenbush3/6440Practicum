bf826f92cb58198d1daf444a3d4549f45d499724
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-11-12 08:43:09 -0500

    j

--- js/data.js
+++ js/data.js
@@ -106,8 +106,8 @@ FHIR.oauth2.ready().then(function(client) {
       } else {
         document.getElementById('covid_vaccine').innerHTML = 'No Recent Vaccine';
       }
-      if(imm.entry[i].resource.vaccineCode.coding[i].code == '150'){
-        flu_vaccine = new Date(imm.entry[0].resource.date);
+      if(imm.entry[i].resource.vaccineCode.coding[i].code == '140'){
+        flu_vaccine = new Date(imm.entry[0].resource.occurenceDateTime);
         document.getElementById('flu_vaccine').innerHTML = flu_vaccine.toDateString();
       } else {
         document.getElementById('flu_vaccine').innerHTML = 'No Recent Vaccine';
