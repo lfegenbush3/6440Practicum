772765624ca367e4b852ea3fa5c7a344737b6f42
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-01 11:20:56 -0500

    dfa

--- js/data.js
+++ js/data.js
@@ -187,7 +187,7 @@ FHIR.oauth2.ready().then(function(client) {
     function addNewWeight() {
       var annotation = "test annotation"
       console.log(annotation)
-      console.log(client.patient.id)
+      console.log("client " + client.patient.id)
       client.update({resource: client.patient.id}).done(function(w){
           
         }); 
