3d7a1f54512df4958504fce9f720498d049580e1
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-01 10:34:30 -0500

    testing

--- js/data.js
+++ js/data.js
@@ -183,4 +183,13 @@ FHIR.oauth2.ready().then(function(client) {
       
 
     });
+
+    function addNewWeight() {
+      var annotation = "test annotation"
+      console.log(annotation)
+    }
+  
+    //event listner when the add button is clicked to call the function that will add the note to the weight observation
+    document.getElementById('add').addEventListener('click', addNewWeight);
+  
   }).catch(console.error);
