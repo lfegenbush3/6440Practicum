8347f82a219b9de65c837c025424245f4345c319
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-03 16:14:35 -0500

    xz

--- js/data.js
+++ js/data.js
@@ -93,7 +93,7 @@ FHIR.oauth2.ready().then(function(client) {
     }
   );
  // get immunization resoruce values
-  client.request(`Procedure?patient=${client.patient.id}`, {
+  client.request(`Immunization?patient=${client.patient.id}`, {
     resolveReferences: ['identifier', 'status']
   }).then(function(imm){
     console.log(imm)
@@ -124,7 +124,7 @@ FHIR.oauth2.ready().then(function(client) {
   );
 
   // get procedure resource values 
-  client.request(`Immunization?patient=${client.patient.id}`, {
+  client.request(`Procedure?patient=${client.patient.id}`, {
     resolveReferences: ['identifier', 'status']
   }).then(function(proc){
       console.log(proc.entry[0][0].resource)
