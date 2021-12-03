c52f28e63e183d0ab042038b8a5f26d29a5e7e9b
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-03 18:06:38 -0500

    sdaf

--- js/data.js
+++ js/data.js
@@ -101,17 +101,21 @@ FHIR.oauth2.ready().then(function(client) {
       for(i = 0; i < imm.entry.length; i++){
         console.log(imm.entry[i])
         console.log('here')
-        if(imm.entry[i].resource.vaccineCode.coding[i].code == '208'){
-          covid_vaccine = new Date(imm.entry[0].resource.date);
-          document.getElementById('covid_vaccine').innerHTML = covid_vaccine.toDateString();
-        } else {
-          document.getElementById('covid_vaccine').innerHTML = 'No Recent Vaccine';
-        }
-        if(imm.entry[i].resource.vaccineCode.coding[i].code == '140'){
-          flu_vaccine = new Date(imm.entry[0].resource.occurrenceDateTime);
-          document.getElementById('flu_vaccine').innerHTML = flu_vaccine.toDateString();
-        } else {
-          document.getElementById('flu_vaccine').innerHTML = 'No Recent Vaccine';
+        try{
+          if(imm.entry[i].resource.vaccineCode.coding[i].code == '208'){
+            covid_vaccine = new Date(imm.entry[0].resource.date);
+            document.getElementById('covid_vaccine').innerHTML = covid_vaccine.toDateString();
+          } else {
+            document.getElementById('covid_vaccine').innerHTML = 'No Recent Vaccine';
+          }
+          if(imm.entry[i].resource.vaccineCode.coding[i].code == '140'){
+            flu_vaccine = new Date(imm.entry[0].resource.occurrenceDateTime);
+            document.getElementById('flu_vaccine').innerHTML = flu_vaccine.toDateString();
+          } else {
+            document.getElementById('flu_vaccine').innerHTML = 'No Recent Vaccine';
+          } 
+        } catch(err) {
+            console.log(err.message)
         }
       }
     } catch(err) {
