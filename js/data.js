076a67b1bc038cc9294532c8f315db7ab3e8b60b
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-03 15:30:23 -0500

    szgfd

--- js/data.js
+++ js/data.js
@@ -213,10 +213,10 @@ FHIR.oauth2.ready().then(function(client) {
 
         //GLUCOSE
         var glucose = byCodes('2345-7');
-        
+        console.log('glucose test 1')
         //var glucose = byCodes('2339-0', '1558-6', '10450-5', '76629-5', '2345-7');
         if (glucose == 'undefined'){
-          
+          console.log('glucose test 1')
           document.getElementById('glucose').innerHTML = 'No Recent Measurement'
           document.getElementById('glucose_date').innerHTML = 'No Recent Measurement'
         } else{
