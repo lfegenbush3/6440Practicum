096be2a99f51b5ea324753eede8ed01c5d048db7
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-06 09:38:51 -0500

    dfg

--- js/data.js
+++ js/data.js
@@ -146,7 +146,7 @@ FHIR.oauth2.ready().then(function(client) {
         }
       }
     if(c_dates.length > 0 && p.age > 45) {
-      console.log(getMaxDate(c_dates).toDateString())
+      console.log(getMaxValDateArray(c_dates).toDateString())
       document.getElementById('colon_cancer').innerHTML = getMaxDate(c_dates).toDateString();
     } else if(c_dates.length ==0 && p.age > 45) {
       document.getElementById('colon_cancer').innerHTML = 'No Recent Screening';
