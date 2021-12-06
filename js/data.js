d1a3b14b71c4081c6b5ecc4933579f4d39d3a5bd
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-05 19:24:59 -0500

    sdfa

--- js/data.js
+++ js/data.js
@@ -115,7 +115,7 @@ FHIR.oauth2.ready().then(function(client) {
           }
           if(imm.entry[i].resource.vaccineCode.coding[0].code == '140'){
             //console.log(new Date(imm.entry[i].resource.occurrenceDateTime))
-            flu_vaccine.push(imm.entry[i].resource.occurrenceDateTime);
+            flu_vaccine.push(new Date(imm.entry[i].resource.occurrenceDateTime));
             console.log(flu_vaccine)
             console.log(getMaxValDateArray(flu_vaccine))
             document.getElementById('flu_vaccine').innerHTML = flu_vaccine[1].toDateString();
