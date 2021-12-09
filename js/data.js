d8e1a79f202e27ba68f6de5ae8c5476f70043633
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-09 10:34:25 -0500

    testing

--- js/data.js
+++ js/data.js
@@ -114,7 +114,7 @@ FHIR.oauth2.ready().then(function(client) {
             document.getElementById('covid_vaccine').innerHTML = covid_vaccine.toDateString();
           } 
           if(imm.entry[i].resource.vaccineCode.coding[0].code == '140'){
-            console.log(new Date(imm.entry[i].resource.occurrenceDateTime))
+            console.log(imm.entry[i].resource.occurrenceDateTime)
             flu_vaccine.push(new Date(imm.entry[i].resource.occurrenceDateTime));
             document.getElementById('flu_vaccine').innerHTML = getMaxValDateArray(flu_vaccine).toDateString();
           } 
