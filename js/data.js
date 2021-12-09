ae657f979fdf8c84315bd9aad429291f366644a1
lfegenbush3 <67565578+lfegenbush3@users.noreply.github.com>
2021-12-09 13:29:31 -0500

    testing

--- js/data.js
+++ js/data.js
@@ -131,19 +131,15 @@ FHIR.oauth2.ready().then(function(client) {
             document.getElementById('shingles_label').style.display = "none"
             document.getElementById('shingles_vaccine').style.display = "none"
           }
-          if(p.age > 9){
-            if(imm.entry[i].resource.vaccineCode.coding[0].code == '62'){
+          if(p.age > 9 && imm.entry[i].resource.vaccineCode.coding[0].code == '62'){
               hpv_vaccine.push(new Date(imm.entry[i].resource.occurrenceDateTime));
               document.getElementById('hpv_vaccine').innerHTML = getMaxValDateArray(hpv_vaccine).toDateString();
-            } else {
+          } else {
               console.log("hpv: " + hpv_vaccine)
               document.getElementById('hpv_label').style.display = "none"
               document.getElementById('hpv_vaccine').style.display = "none"
-            }
-          } else {
-            document.getElementById('hpv_label').style.display = "none"
-            document.getElementById('hpv_vaccine').style.display = "none"
           }
+         
           
           if(flu_vaccine == "undefined"|| flu_vaccine == ""  ||flu_vaccine =='Invalid Date') {
             document.getElementById('flu_vaccine').innerHTML = 'No Recent Vaccine';
